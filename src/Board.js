97699d3902c561f02788df30be0ffc71306d2e1a
Albin Blent <albin.blent@ffcg.se>
2018-10-25 22:10:49 +0200

    Almost working base

--- src/Board.js
+++ src/Board.js
@@ -30,73 +30,171 @@ export function calculateWinner(squares) {
   return null;
 }
 
-function updateVerticaly(squares){
-    squares = squares.slice()
-    const boardSize = squares.length
-    for (let colIndex = 0; colIndex < boardSize; colIndex++) {
-        const row = squares[colIndex];
-        let firstSquare = {type: null, index: null}
-        let lastSquare = {type: null, index: null}
-        for (let index = 0; index < row.length; index++) {
-            const element = row[index];
-            if(element === 'X' || element === 'O') {
-                firstSquare.type = element
-                firstSquare.index = index
-                break
-            }
-        }
-        if (firstSquare !== 'X' || firstSquare !== 'O') {
-            break
-        }
-        for (let index = row.length; index >= 0; index--) {
-            const element = row[index];
-            if(element === 'X' || element === 'O') {
-                lastSquare.type = element
-                lastSquare.index = index
-                if (firstSquare.type === lastSquare.type) {
-                    for (let index = firstSquare.index; index < lastSquare.index; index++) {
-                        row[index] = firstSquare.type
-                    }
-                    break
+export function flipBetweenIndex(indexFrom, indexTo, array, squareType) {
+    array = array.slice()
+
+    for (let indexToFlip = indexFrom; indexToFlip < indexTo; indexToFlip++) {
+        array[indexToFlip] = squareType
+    }
+    return array
+}
+
+export function updateRow(row){
+    row = row.slice()
+    for (let index = 0; index < row.length; index++) {
+        if (row[index]) {
+            const firstNonNullElement = row[index]
+            for (let indexRight = row.length; indexRight > index; indexRight--) {
+                const elementFromTheRight = row[indexRight]
+                if (firstNonNullElement === elementFromTheRight) {
+                    return flipBetweenIndex(index, indexRight, row, firstNonNullElement)
                 }
             }
         }
-        squares[colIndex] = row
+        
     }
-    return squares
+    return row
+}
+/*
+(0,2):  (1,3)
+(0,1):  (2,3) -> (1,2)
+(0,0):  (3,3) -> (2,2) -> (1,1)
+(1,0):  (3,2) -> (2,1)
+(2,0):  (3,1) 
+
+i = 0 -> 1 -> 2 // tills y + i = length || x + i = length --> i < length - y
+const elementToCompare = board[length - y - i][length - x - i]
+
+  x ------>
+y [O][O][X][X]
+| [X][O][X][X]
+| [X][X][O][X]
+v [X][X][O][X]
+
+(0,1): (1,0)
+(0,2): (2,0) -> (1,1)
+(0,3): (3,0) -> (2,1) -> (1,2)
+(1,3): (3,1) -> (2,2)
+(2,3): (3,2)
+
+i = 0 -> 1 -> // while x < y - i
+constElementToCompare = board[y - i][x + i]
+*/
+
+export function updateDiagonaly(board) {
+    let resultBoard = board.slice()
+    console.log(resultBoard)
+    let flip = false
+    const boardLength = board.length - 1
+    const x = 0
+    for (let y = 0; y < boardLength; y++) {
+        const square = board[x].slice()[y]
+        for (let i = 0; i < (boardLength - y); i++) {
+            if (flip) {
+                resultBoard[boardLength - y - i][boardLength - x - i] = square
+            } else {
+                const squareToCompare = board[boardLength - y - i][boardLength - x - i]
+                flip = (square !== null && squareToCompare !== null && square === squareToCompare)
+            }
+        }
+    }
+    flip = false
+    const y = 0
+    for (let x = 0; x < boardLength; x++) {
+        const square = board[x].slice()[y]
+        for (let i = 0; i < (boardLength - x); i++) {
+            if (flip) {
+                resultBoard[boardLength - y - i][boardLength - x - i] = square
+            } else {
+                const squareToCompare = board[boardLength - y - i][boardLength - x - i]
+                flip = (square !== null && squareToCompare !== null && square === squareToCompare)
+            }
+        }
+    }
+    flip = false
+    const x2 = 0
+    for (let y = 0; y < boardLength; y++) {
+        const square = board[x2].slice()[y]
+        for (let i = 0; i < y; i++) {
+            if (flip) {
+                resultBoard[y - i][x + i] = square
+            } else {
+                const squareToCompare = board[y - i][x + i]
+                flip = (square !== null && squareToCompare !== null && square === squareToCompare)
+            }
+        }
+    }
+    flip = false
+    const y2 = 0
+    for (let x = 0; x < boardLength; x++) {
+        const square = board[x].slice()[y2]
+        for (let i = 0; i < (boardLength - x); i++) {
+            if (flip) {
+                resultBoard[boardLength - y2 - i][boardLength - x - i] = square
+            } else {
+                const squareToCompare = board[boardLength - y2 - i][boardLength - x - i]
+                flip = (square !== null && squareToCompare !== null && square === squareToCompare)
+            }
+        }
+    }
+    console.log('--------')
+    console.log(resultBoard)
+    return resultBoard
 }
 
-export function updateBoardAfterMarkerPlaced(squares) {
-    squares = squares.slice()
-    return updateVerticaly(squares)
+function createBoard(length) {
+    return Array(length).fill(Array(length).fill(null))
+}
+
+export function flipBoard(board) {
+    board = board.slice()
+    let flippedBoard = createBoard(board.length)
+    for (let x = 0; x < board.length; x++) {
+        for (let y = 0; y < board.length; y++) {
+            const value = board[x].slice()[y]
+            let row = flippedBoard[y].slice()
+            row[x] = value
+            flippedBoard[y] = row.slice()
+        }
+    }
+    return flippedBoard
+}
+
+export function updateBoardAfterMarkerPlaced(board) {
+    const updatedHirizontal = board.map(row => updateRow(row))
+    const flipped = flipBoard(updatedHirizontal)
+    const updatedVerticaly = flipped.map(col => updateRow(col))
+    const updatedBoard = flipBoard(updatedVerticaly)
+    return updatedBoard
 }
 
 export default class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-        squares: Array(9).fill(null),
-        xIsNext: true,
+            squares: Array(9).fill(Array(9).fill(null)),
+            xIsNext: true,
         };
     }
 
-    handleClick(i) {
+    handleClick(x, y) {
+        // ToDo man får bara placera bredvid andra redan placerade rutor
         const squares = this.state.squares.slice();
-        if (calculateWinner(squares) || squares[i]) {
-        return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        const squaresX = this.state.squares[x].slice();
+        squaresX[y] = this.state.xIsNext ? 'X' : 'O';
+        squares[x] = squaresX
+        const updatedSquares = updateBoardAfterMarkerPlaced(squares)
         this.setState({
-        squares: squares,
-        xIsNext: !this.state.xIsNext,
+            squares: updatedSquares,
+            xIsNext: !this.state.xIsNext,
         });
     }
 
-    renderSquare(i) {
+    renderSquare(x, y) {
         return (
         <Square
-            value={this.state.squares[i]}
-            onClick={() => this.handleClick(i)}
+            value={this.state.squares[x][y]}
+            onClick={() => this.handleClick(x, y)}
         />
         );
     }
@@ -111,10 +209,10 @@ export default class Board extends React.Component {
         }
 
         let rows = []
-        for (let i = 0; i < 8; i++) {
+        for (let x = 0; x < 8; x++) {
             let col = []
             for (let y = 0; y < 8; y++) {
-            col.push(this.renderSquare(i + y))
+            col.push(this.renderSquare(x, y))
             }
             rows.push(<div className="board-row">{col}</div>)
         }
