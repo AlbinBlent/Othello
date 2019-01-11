1f7a4d24be25e46511914860b5b2fee1abb0afd6
Robin Wikström <wikstrom.robin@live.se>
2019-01-12 00:57:18 +0100

    fixed undefined player, indentations & removed alert

--- client/src/components/lobby/Lobby.jsx
+++ client/src/components/lobby/Lobby.jsx
@@ -1,42 +1,42 @@
-import React from 'react';
-import io from 'socket.io-client';
+import React from "react";
+import io from "socket.io-client";
 
 export default class Lobby extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            player: {},  
-            players: []
-        };
-        const options = {query: 'name='+JSON.stringify(this.props.location.state.name)}
-        const socket = io('http://localhost:3001/', options)
-        socket.on('currentPlayers', (players) => {
-            this.setState({players: players})
-        })
-        socket.on('newPlayer', (player) => {
-          const players = this.state.players.slice()
-          players.push(player)
-          this.setState({players: players})
-        })
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      player: this.props.location.state.name,
+      players: []
+    };
+    const options = { query: "name=" + JSON.stringify(this.state.player) };
+    const socket = io("http://localhost:3001/", options);
+    socket.on("currentPlayers", players => {
+      this.setState({ players: players });
+    });
+    socket.on("newPlayer", player => {
+      const players = this.state.players.slice();
+      players.push(player);
+      this.setState({ players: players });
+    });
+  }
 
-    renderPlayers(players) {
-       console.log(players)
-        const playerList = []
-        players.forEach(player => {
-            playerList.push(<li>{player.name}</li>)
-        });
-        return playerList
-    }
+  renderPlayers(players) {
+    console.log(players);
+    const playerList = [];
+    players.forEach(player => {
+      playerList.push(<li>{player.name}</li>);
+    });
+    return playerList;
+  }
 
-    render() {
-        const { players, player } = this.state
-        return(
-            <div>
-                <h2>Players in lobby:</h2> 
-            <ul>{this.renderPlayers(players)}</ul>
-            <h1>{'You: ' + player.name}</h1>
-            </div>
-        );
-    }
+  render() {
+    const { players, player } = this.state;
+    return (
+      <div>
+        <h2>Players in lobby:</h2>
+        <ul>{this.renderPlayers(players)}</ul>
+        <h1>{"You: " + player}</h1>
+      </div>
+    );
+  }
 }
--- client/src/components/startpage/Home.jsx
+++ client/src/components/startpage/Home.jsx
@@ -1,37 +1,46 @@
-import React from 'react'
+import React from "react";
 
 export default class Home extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {name: ''};
+    this.state = { name: "" };
+    this.state = { validInput: true };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    }
+  }
 
-    handleChange(event) {
-      this.setState({name: event.target.value});
-    }
+  handleChange(event) {
+    this.setState({ name: event.target.value });
+  }
 
-    handleSubmit() {
-      const name = this.state.name;
-      const letters = /^[A-Öa-ö]+$/;
-      if (name.length > 0 && name.match(letters)) {
-        this.props.history.push('/lobby', { name});
-      } else {
-        alert('You did not enter a name! Only letters allowed.');
-      }
+  handleSubmit() {
+    const name = this.state.name;
+    const letters = /^[A-Öa-ö]+$/;
+    if (name.length > 0 && name.match(letters)) {
+      this.props.history.push("/lobby", { name });
+    } else {
+      this.setState({ validInput: false });
     }
+  }
 
-    render() {
-      return(
+  render() {
+    if (this.state.validInput === true) {
+      return (
         <form onSubmit={this.handleSubmit}>
           <label>
             Enter name:
-            <input type="text" value={this.state.name} onChange={this.handleChange} />
+            <input
+              type="text"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
           </label>
           <input type="submit" value="Submit" />
         </form>
       );
+    } else {
+      return <h1>FEL INPUT! (Hur gör jag detta snyggare..)</h1>;
     }
   }
+}
